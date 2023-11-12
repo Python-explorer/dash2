4a1ce00d6c31f40dbdcae2c3b15b3ea45cc4996b
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 16:26:20 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -5,20 +5,25 @@ def risk_table():
     # Load the CSV file into a pandas DataFrame
     df = pd.read_csv('https://raw.githubusercontent.com/Python-explorer/dash2/main/risk/data.csv')
     
-    # Define a function to apply coloring to risk score
+    # Define a function to apply coloring to 'Total risk score'
     def color_risk_score(val):
+        color = 'green'  # default color
         if val >= 20:
             color = 'red'
         elif 11 <= val < 20:
             color = 'orange'
-        else:
-            color = 'green'
-        return f'background-color: {color}; color: white;'
+        return f'color: white; background-color: {color};'
 
     # Apply the styling
     styled_df = df.style.applymap(color_risk_score, subset=['Total risk score'])\
-        .set_properties(**{'background-color': 'grey', 'color': 'black'}, subset=pd.IndexSlice[0:0, :])\
-        .set_table_styles([{'selector': 'th', 'props': [('background-color', 'grey'), ('color', 'black'), ('font-weight', 'bold')]}])
-    
-    # Display the DataFrame as a table in Streamlit with styling
-    st.dataframe(styled_df)
+        .set_table_styles([{
+            # This styles the header row
+            'selector': 'thead th',
+            'props': [('background-color', 'grey'), ('color', 'black'), ('font-weight', 'bold')]
+        }])
+
+    # Convert the styled DataFrame to HTML and display using st.markdown
+    st.markdown(styled_df.to_html(escape=False), unsafe_allow_html=True)
+
+# Call the risk_table function to display the table
+risk_table()
