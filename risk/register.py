e76ea8d0907fcc21bbc136f408d2cda3f5ed93b2
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 16:21:40 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,26 +1,39 @@
 import streamlit as st
 import pandas as pd
 
-st.markdown(styled_df.to_html(escape=False), unsafe_allow_html=True)
-
 def risk_table():
     # Load the CSV file into a pandas DataFrame
     df = pd.read_csv('https://raw.githubusercontent.com/Python-explorer/dash2/main/risk/data.csv')
     
     # Define a function to apply coloring to risk score
     def color_risk_score(val):
+        color = 'green'  # default color
         if val >= 20:
             color = 'red'
         elif 11 <= val < 20:
             color = 'orange'
-        else:
-            color = 'green'
-        return f'background-color: {color}; color: white;'
+        # Note: 'white' text color for readability
+        return f'color: white; background-color: {color};'
 
     # Apply the styling
     styled_df = df.style.applymap(color_risk_score, subset=['risk score'])\
-        .set_properties(**{'background-color': 'grey', 'color': 'black'}, subset=pd.IndexSlice[0:0, :])\
-        .set_table_styles([{'selector': 'th', 'props': [('background-color', 'grey'), ('color', 'black'), ('font-weight', 'bold')]}])
-    
-    # Display the DataFrame as a table in Streamlit with styling
-    st.dataframe(styled_df)
+        .set_table_styles([{
+            'selector': 'th',
+            'props': [('background-color', 'grey'), ('color', 'black'), ('font-weight', 'bold')]
+        }, {
+            # Ensure index cells (headers of rows) are also styled
+            'selector': '.index_name',
+            'props': [('display', 'none')]  # This hides the index name if it's showing
+        }, {
+            'selector': '.row_heading',
+            'props': [('background-color', 'grey'), ('color', 'black')]
+        }, {
+            'selector': '.blank',
+            'props': [('background-color', 'grey'), ('color', 'black')]
+        }])
+
+    # Convert the styled DataFrame to HTML and display using st.markdown
+    st.markdown(styled_df.to_html(escape=False), unsafe_allow_html=True)
+
+# Call the risk_table function to display the table
+risk_table()
