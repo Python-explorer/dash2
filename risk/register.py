9e8d78b464df6fa394f9416b4ac6687bd0d9ac5f
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 16:19:08 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -1,11 +1,24 @@
 import streamlit as st
 import pandas as pd
 
-import streamlit as st
-
 def risk_table():
     # Load the CSV file into a pandas DataFrame
     df = pd.read_csv('https://raw.githubusercontent.com/Python-explorer/dash2/main/risk/data.csv')
     
-    # Display the DataFrame as a table in Streamlit
-    st.dataframe(df)
+    # Define a function to apply coloring to risk score
+    def color_risk_score(val):
+        if val >= 20:
+            color = 'red'
+        elif 11 <= val < 20:
+            color = 'orange'
+        else:
+            color = 'green'
+        return f'background-color: {color}; color: white;'
+
+    # Apply the styling
+    styled_df = df.style.applymap(color_risk_score, subset=['risk score'])\
+        .set_properties(**{'background-color': 'grey', 'color': 'black'}, subset=pd.IndexSlice[0:0, :])\
+        .set_table_styles([{'selector': 'th', 'props': [('background-color', 'grey'), ('color', 'black'), ('font-weight', 'bold')]}])
+    
+    # Display the DataFrame as a table in Streamlit with styling
+    st.dataframe(styled_df)
