a9359dbada4b575799c2f7557022508962b4e1fd
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 16:14:01 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -4,17 +4,8 @@ import pandas as pd
 import streamlit as st
 
 def risk_table():
-    st.text('test')
-# Raw URL of the CSV file in the GitHub repository
-# csv_url = 'https://raw.githubusercontent.com/Python-explorer/demodashboard/main/risk/data.csv'
-
-# Load the CSV file into a DataFrame and ensure proper data types, if necessary
-# df = pd.read_csv(csv_url, dtype={"text_column1": str, "text_column2": str, "num_column1": float, "num_column2": float, "num_column3": float})
-
-# Display the DataFrame as a table in Streamlit to debug
-# st.write("Displaying the first two rows of the DataFrame:")
-# st.write(df.head(2))
-
-# Display the DataFrame as a table in Streamlit
-# st.write("Displaying the full DataFrame:")
-# st.table(df)
+    # Load the CSV file into a pandas DataFrame
+    df = pd.read_csv('https://raw.githubusercontent.com/Python-explorer/dash2/main/risk/data.csv')
+    
+    # Display the DataFrame as a table in Streamlit
+    st.dataframe(df)
