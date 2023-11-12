22dfb9284fc3538259e6409e2807af6bd3cd9ec2
Python_explorer <70327477+Python-explorer@users.noreply.github.com>
2023-11-12 16:24:41 +0000

    Update register.py

--- risk/register.py
+++ risk/register.py
@@ -7,33 +7,18 @@ def risk_table():
     
     # Define a function to apply coloring to risk score
     def color_risk_score(val):
-        color = 'green'  # default color
         if val >= 20:
             color = 'red'
         elif 11 <= val < 20:
             color = 'orange'
-        # Note: 'white' text color for readability
-        return f'color: white; background-color: {color};'
+        else:
+            color = 'green'
+        return f'background-color: {color}; color: white;'
 
     # Apply the styling
-    styled_df = df.style.applymap(color_risk_score, subset=['risk score'])\
-        .set_table_styles([{
-            'selector': 'th',
-            'props': [('background-color', 'grey'), ('color', 'black'), ('font-weight', 'bold')]
-        }, {
-            # Ensure index cells (headers of rows) are also styled
-            'selector': '.index_name',
-            'props': [('display', 'none')]  # This hides the index name if it's showing
-        }, {
-            'selector': '.row_heading',
-            'props': [('background-color', 'grey'), ('color', 'black')]
-        }, {
-            'selector': '.blank',
-            'props': [('background-color', 'grey'), ('color', 'black')]
-        }])
-
-    # Convert the styled DataFrame to HTML and display using st.markdown
-    st.markdown(styled_df.to_html(escape=False), unsafe_allow_html=True)
-
-# Call the risk_table function to display the table
-risk_table()
+    styled_df = df.style.applymap(color_risk_score, subset=['Total risk score'])\
+        .set_properties(**{'background-color': 'grey', 'color': 'black'}, subset=pd.IndexSlice[0:0, :])\
+        .set_table_styles([{'selector': 'th', 'props': [('background-color', 'grey'), ('color', 'black'), ('font-weight', 'bold')]}])
+    
+    # Display the DataFrame as a table in Streamlit with styling
+    st.dataframe(styled_df)
